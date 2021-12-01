6d9feab607740a7d5bad741c1ef829ec1ce98cc8
Agrael11 <Oliver.Neuschl@gmail.com>
2021-12-01 16:00:18 +0100

    Add project files.

---
+++ Advent-Of-Code-2021-1-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode.Day1.Puzzle1
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader sr = new StreamReader("inputData.txt");
+            string[] inputData = sr.ReadToEnd().Split('\n');
+            
+            int prevNumber = int.Parse(inputData[0]);
+            int increaseCount = 0;
+
+            for (int i = 1; i < inputData.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(inputData[i])) break;
+
+                int number = int.Parse(inputData[i]);
+
+                Console.Write($"{prevNumber}->{number}: ");
+
+                if (number > prevNumber)
+                {
+                    Console.WriteLine("(Increased)");
+                    increaseCount++;
+                }
+                else
+                {
+                    Console.WriteLine("(Decreased or equal)");
+                }
+                prevNumber = number;
+            }
+
+            Console.WriteLine(increaseCount);
+        }
+    }
+}
---
+++ Advent-Of-Code-2021-1-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode.Day1.Puzzle1
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader sr = new StreamReader("inputData.txt");
+            string[] inputData = sr.ReadToEnd().Split('\n');
+
+            int prevSum = GetSum(0, ref inputData);
+            int increaseCount = 0;
+
+            for (int i = 1; i < inputData.Length - 3; i++)
+            {
+                int number = GetSum(i, ref inputData);
+
+                Console.Write($"{prevSum}->{GetSumText(i, ref inputData)} = {number}: ");
+
+                if (number > prevSum)
+                {
+                    Console.WriteLine("(Increased)");
+                    increaseCount++;
+                }
+                else
+                {
+                    Console.WriteLine("(Decreased or equal)");
+                }
+                prevSum = number;
+            }
+            Console.WriteLine(increaseCount);
+        }
+
+        static int GetSum(int index, ref string[] inputData)
+        {
+            return int.Parse(inputData[index]) + int.Parse(inputData[index + 1]) + int.Parse(inputData[index + 2]);
+        }
+
+        static string GetSumText(int index, ref string[] inputData)
+        {
+            return $"{inputData[index]} + {inputData[index + 1]} + {inputData[index + 2]}";
+        }
+    }
+}
