2a9aebc77303626fa048ddc584c02a5a3b5be5f0
Agrael11 <Oliver.Neuschl@gmail.com>
2021-12-07 13:15:38 +0100

    7th day of christmas

---
+++ Advent-Of-Code-2021-7-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day7.Puzzle1
+{
+    /// <summary>
+    /// Main Class for Day 7 - Challange 1 of Advent of Code 2021
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            //Read input data from text file and splits them by line
+            List<string> inputData = new();
+            using (StreamReader sr = new("inputData.txt"))
+            {
+                inputData = sr.ReadToEnd().Replace("\r", "").TrimEnd('\n').Split('\n').ToList();
+            }
+
+            //Parse data into crabmarines and get their biggest position
+            List<int> crabMarines = new ();
+            int maxHorizontal = 0;
+            foreach (string inputLine in inputData)
+            {
+                foreach (string inputNumber in inputLine.Split(','))
+                {
+                    int crabMarine = int.Parse(inputNumber);
+                    crabMarines.Add(crabMarine);
+                    if (maxHorizontal < crabMarine) maxHorizontal = crabMarine;
+                }
+            }
+
+            //Find least amount of fuel required to move them to same position
+            int minFuel = int.MaxValue;
+            for (int i = 0; i < maxHorizontal; i++)
+            {
+                int fuel = 0;
+                foreach (int crab in crabMarines)
+                {
+                    fuel += Math.Abs(crab - i);
+                }
+                if (fuel < minFuel) minFuel = fuel;
+            }
+
+            Console.WriteLine(minFuel);
+        }
+    }
+}
