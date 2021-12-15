3fac8e6a4c4149c995164feb198523aef0da4dc3
Agrael11 <Oliver.Neuschl@gmail.com>
2021-12-15 11:37:53 +0100

    Just made some refactoring on time formatting.

--- Advent-Of-Code-2021/Program.cs
+++ Advent-Of-Code-2021/Program.cs
@@ -15,40 +15,43 @@ namespace AdventOfCode
             while (result != "Q")
             {
                 Console.WriteLine("Advent of Code 2021!");
-                Console.Write("Select the challange [1-14] or all challanges [A], write [Q] to quit: ");
-                result = Console.ReadLine()??"";
+                Console.Write("Select the challange [1-15] or all challanges [A], write [Q] to quit: ");
+                result = Console.ReadLine() ?? "";
 
-                switch (result.ToUpper())
+                if (int.TryParse(result, out int parsed) && parsed >= 1 && parsed <= 15)
                 {
-                    case "1":
-                    case "2":
-                    case "3":
-                    case "4":
-                    case "5":
-                    case "6":
-                    case "7":
-                    case "8":
-                    case "9":
-                    case "10":
-                    case "11":
-                    case "12":
-                    case "13":
-                    case "14":
-                        DoChallange(int.Parse(result));
-                        break;
-                    case "A":
-                        DoAllChallanges();
-                        break;
-                    case "Q":
-                        break;
-                    default:
-                        DrawATree();
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("Wrong command.");
-                        break;
+                    DoChallange(parsed);
+                }
+                else if (result.ToUpper() == "A")
+                {
+                    DoAllChallanges();
+                    break;
+                }
+                else if (result.ToUpper() != "Q")
+                {
+                    DrawATree();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Wrong command.");
                 }
             }
             Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static private string FormatTime(ulong milliseconds)
+        {
+            uint milli = (uint)(milliseconds % 1000);
+            uint seconds = (uint)(milliseconds / 1000);
+            uint minutes = seconds / 60;
+            seconds %= 60;
+            uint hours = minutes / 60;
+            minutes %= 60;
+            string time = "";
+            if (hours > 0) time = hours.ToString().PadLeft(2, '0') + ":";
+            if (minutes > 0 || hours > 0) time += minutes.ToString().PadLeft(2, '0') + ":";
+            if (seconds > 0 && (minutes > 0 || hours > 0)) time += seconds.ToString().PadLeft(2, '0') + "." + milli.ToString().PadLeft(3, '0');
+            else if (seconds > 0) time += seconds.ToString().PadLeft(2, '0') + "." + milli.ToString().PadLeft(3, '0') + "s";
+            else time += milli.ToString().PadLeft(3, '0') + "ms";
+            return time;
         }
 
         static ulong DoChallange(int challange, bool nice = true)
@@ -60,133 +63,29 @@ namespace AdventOfCode
             string inputData = File.ReadAllText($"inputData{challange}.txt");
             Stopwatch watch1;
             Stopwatch watch2;
-            string result1;
-            string result2;
-            switch (challange)
-            {
-                case 1:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day01.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day01.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 2:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day02.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day02.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 3:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day03.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day03.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 4:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day04.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day04.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 5:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day05.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day05.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 6:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day06.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day06.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 7:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day07.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day07.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 8:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day08.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day08.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 9:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day09.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day09.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 10:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day10.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day10.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 11:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day11.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day11.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 12:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day12.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day12.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 13:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day13.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day13.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                case 14:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = new Day14.Challange1().DoChallange(inputData).ToString();
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = new Day14.Challange2().DoChallange(inputData).ToString();
-                    watch2.Stop();
-                    break;
-                default:
-                    watch1 = Stopwatch.StartNew();
-                    result1 = "ERROR";
-                    watch1.Stop();
-                    watch2 = Stopwatch.StartNew();
-                    result2 = "ERROR";
-                    watch2.Stop();
-                    break;
-            }
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write($"Results of Day {challange} are: ");
+            watch1 = Stopwatch.StartNew();
+            string result1 = challange switch
+            {
+                1 => new Day01.Challange1().DoChallange(inputData).ToString(),
+                2 => new Day02.Challange1().DoChallange(inputData).ToString(),
+                3 => new Day03.Challange1().DoChallange(inputData).ToString(),
+                4 => new Day04.Challange1().DoChallange(inputData).ToString(),
+                5 => new Day05.Challange1().DoChallange(inputData).ToString(),
+                6 => new Day06.Challange1().DoChallange(inputData).ToString(),
+                7 => new Day07.Challange1().DoChallange(inputData).ToString(),
+                8 => new Day08.Challange1().DoChallange(inputData).ToString(),
+                9 => new Day09.Challange1().DoChallange(inputData).ToString(),
+                10 => new Day10.Challange1().DoChallange(inputData).ToString(),
+                11 => new Day11.Challange1().DoChallange(inputData).ToString(),
+                12 => new Day12.Challange1().DoChallange(inputData).ToString(),
+                13 => new Day13.Challange1().DoChallange(inputData).ToString(),
+                14 => new Day14.Challange1().DoChallange(inputData).ToString(),
+                15 => new Day15.Challange1().DoChallange(inputData).ToString(),
+                _ => "ERROR",
+            };
+            watch1.Stop();
             Console.ForegroundColor = ConsoleColor.Cyan;
             if (result1.Contains('\n') || result1.Length > 20)
             {
@@ -198,6 +97,27 @@ namespace AdventOfCode
                 Console.Write(result1);
             }
             Console.ForegroundColor = ConsoleColor.White;
+            watch2 = Stopwatch.StartNew();
+            string result2 = challange switch
+            {
+                1 => new Day01.Challange2().DoChallange(inputData).ToString(),
+                2 => new Day02.Challange2().DoChallange(inputData).ToString(),
+                3 => new Day03.Challange2().DoChallange(inputData).ToString(),
+                4 => new Day04.Challange2().DoChallange(inputData).ToString(),
+                5 => new Day05.Challange2().DoChallange(inputData).ToString(),
+                6 => new Day06.Challange2().DoChallange(inputData).ToString(),
+                7 => new Day07.Challange2().DoChallange(inputData).ToString(),
+                8 => new Day08.Challange2().DoChallange(inputData).ToString(),
+                9 => new Day09.Challange2().DoChallange(inputData).ToString(),
+                10 => new Day10.Challange2().DoChallange(inputData).ToString(),
+                11 => new Day11.Challange2().DoChallange(inputData).ToString(),
+                12 => new Day12.Challange2().DoChallange(inputData).ToString(),
+                13 => new Day13.Challange2().DoChallange(inputData).ToString(),
+                14 => new Day14.Challange2().DoChallange(inputData).ToString(),
+                15 => new Day15.Challange2().DoChallange(inputData).ToString(),
+                _ => "ERROR",
+            };
+            watch2.Stop();
             Console.Write(" and ");
             Console.ForegroundColor = ConsoleColor.Cyan;
             if (result2.Contains('\n') || result2.Length > 20)
@@ -214,33 +134,11 @@ namespace AdventOfCode
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("It took ");
             Console.ForegroundColor = ConsoleColor.Green;
-            uint milli = (uint)(watch1.ElapsedMilliseconds%1000);
-            uint seconds = (uint)(watch1.ElapsedMilliseconds / 1000);
-            uint minutes = seconds / 60;
-            seconds %= 60;
-            uint hours = minutes / 60;
-            minutes %= 60;
-            string time = "";
-            if (hours > 0) time = hours.ToString().PadLeft(2, '0') + ":";
-            if (minutes > 0 || hours > 0) time += minutes.ToString().PadLeft(2, '0') + ":";
-            if (seconds > 0 || minutes > 0 || hours > 0) time += seconds.ToString().PadLeft(2, '0') + ".";
-            time += milli.ToString().PadLeft(3, '0') + " ms";
-            Console.Write(time);
+            Console.Write(FormatTime((ulong)watch1.Elapsed.TotalMilliseconds));
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write(" and ");
             Console.ForegroundColor = ConsoleColor.Green;
-            milli = (uint)(watch2.Elapsed.TotalMilliseconds % 1000);
-            seconds = (uint)(watch2.ElapsedMilliseconds / 1000);
-            minutes = seconds / 60;
-            seconds %= 60;
-            hours = minutes / 60;
-            minutes %= 60;
-            time = "";
-            if (hours > 0) time = hours.ToString().PadLeft(2, '0') + ":";
-            if (minutes > 0 || hours > 0) time += minutes.ToString().PadLeft(2, '0') + ":";
-            if (seconds > 0 || minutes > 0 || hours > 0) time += seconds.ToString().PadLeft(2, '0') + ".";
-            time += milli.ToString().PadLeft(3, '0') + " ms";
-            Console.Write(time);
+            Console.Write(FormatTime((ulong)watch2.Elapsed.TotalMilliseconds));
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(".");
             return (ulong)(watch1.ElapsedMilliseconds + watch2.ElapsedMilliseconds);
@@ -250,7 +148,7 @@ namespace AdventOfCode
         {
             DrawATree();
             ulong totalTime = 0;
-            for (int i = 1; i <= 14; i++)
+            for (int i = 1; i <= 15; i++)
             {
                 totalTime += DoChallange(i, false);
             }
@@ -258,18 +156,7 @@ namespace AdventOfCode
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Total time is: ");
             Console.ForegroundColor = ConsoleColor.Green;
-            uint milli = (uint)(totalTime % 1000);
-            uint seconds = (uint)(totalTime / 1000);
-            uint minutes = seconds / 60;
-            seconds %= 60;
-            uint hours = minutes / 60;
-            minutes %= 60;
-            string time = "";
-            if (hours > 0) time = hours.ToString().PadLeft(2, '0') + ":";
-            if (minutes > 0 || hours > 0) time += minutes.ToString().PadLeft(2, '0') + ":";
-            if (seconds > 0 || minutes > 0 || hours > 0) time += seconds.ToString().PadLeft(2, '0') + ".";
-            time += milli.ToString().PadLeft(3, '0') + " ms";
-            Console.Write(time);
+            Console.Write(FormatTime(totalTime));
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(".");
             Console.WriteLine();
